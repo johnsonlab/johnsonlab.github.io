75f868e052b3cd60761dd190793f3e6d545115de
Josh Johnson <joshua.2.johnson@ucdenver.edu>
2017-04-03 16:08:44 -0600

    test changes
    copied map .py and .ipynb to _talks, re-ran there

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,27 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "London, UK",
+    51.5073219,
+    -0.1276473
+  ],
+  [
+    "Los Angeles, CA",
+    34.054935,
+    -118.2444759
+  ],
+  [
+    "Berkeley CA, USA",
+    37.8708393,
+    -122.2728638
+  ],
+  [
+    "Denver, Colorado",
+    39.7391536,
+    -104.9847033
+  ],
+  [
+    "Aurora, CO",
+    39.729432,
+    -104.8319195
+  ]
+];
