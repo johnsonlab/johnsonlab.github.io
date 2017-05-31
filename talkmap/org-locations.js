f1324f55cd0ed47cbbe580864bcb3c050a840566
Josh Johnson <joshua.2.johnson@ucdenver.edu>
2017-05-31 10:28:21 -0600

    ran talkmap.py within _talks folder

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,28 @@
 var addressPoints = [
   [
-    "Milan, Italy",
-    45.4667971,
-    9.1904984
+    "Pullman, WA",
+    46.7304268,
+    -117.1738949
+  ],
+  [
+    "Jerusalem, Israel",
+    31.78911765,
+    35.2229730135452
+  ],
+  [
+    "Havana, Cuba",
+    23.1379911,
+    -82.3658561
+  ],
+  [
+    "Boston, Massachusetts, USA.",
+    42.3604823,
+    -71.0595677
+  ],
+  [
+    "Denver, Colorado, U.S.A.",
+    39.67228665,
+    -104.87333287908
   ],
   [
     "Edinburgh, Scotland, U.K.",
@@ -15,18 +35,8 @@ var addressPoints = [
     -104.8319195
   ],
   [
-    "Boston, Massachusetts, USA.",
-    42.3604823,
-    -71.0595677
-  ],
-  [
-    "Pullman, WA",
-    46.7304268,
-    -117.1738949
-  ],
-  [
-    "Havana, Cuba",
-    23.1379911,
-    -82.3658561
+    "Milan, Italy",
+    45.4667971,
+    9.1904984
   ]
 ];
