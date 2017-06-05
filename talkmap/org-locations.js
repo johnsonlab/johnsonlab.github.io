b1b96301a474ebb6185237b6f139e51fa9001237
Josh Johnson <joshua.2.johnson@ucdenver.edu>
2017-06-05 15:07:57 -0600

    quick map fix

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,13 +1,13 @@
 var addressPoints = [
   [
-    "Pullman, WA",
-    46.7304268,
-    -117.1738949
-  ],
-  [
     "Jerusalem, Israel",
     31.78911765,
     35.2229730135452
+  ],
+  [
+    "Milan, Italy",
+    45.4667971,
+    9.1904984
   ],
   [
     "Havana, Cuba",
@@ -15,19 +15,19 @@ var addressPoints = [
     -82.3658561
   ],
   [
+    "Edinburgh, Scotland, U.K.",
+    55.9495628,
+    -3.1914971
+  ],
+  [
     "Boston, Massachusetts, USA.",
     42.3604823,
     -71.0595677
   ],
   [
-    "Denver, Colorado, U.S.A.",
-    39.67228665,
-    -104.87333287908
-  ],
-  [
-    "Edinburgh, Scotland, U.K.",
-    55.9495628,
-    -3.1914971
+    "Pullman, WA",
+    46.7304268,
+    -117.1738949
   ],
   [
     "Aurora, CO",
@@ -35,8 +35,8 @@ var addressPoints = [
     -104.8319195
   ],
   [
-    "Milan, Italy",
-    45.4667971,
-    9.1904984
+    "Denver, Colorado, U.S.A.",
+    39.67228665,
+    -104.87333287908
   ]
 ];
